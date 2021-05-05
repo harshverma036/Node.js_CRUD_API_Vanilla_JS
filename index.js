51565667e90efedf91bc34fa071bd82dce656a4f
Harsh Verma <harshverma0362@gmail.com>
2021-05-05 20:15:22 +0530

    html response send

--- index.js
+++ index.js
@@ -2,22 +2,10 @@ const http = require("http");
 const posts = require("./data/posts");
 
 const server = http.createServer((req, res) => {
-  //   res.statusCode = 200;
-  //   res.setHeader("Content-Type", "text/html");
-  //   res.write("<h1>Hello nodejs api</h1>");
-  //   res.end();
-  // INSTEAD OF USING THIS LONG CODE WE CAN ALSO USE
-  // SHORTHAND OF THIS.
-  if (req.url === "/" && req.method === "GET") {
-    res.writeHead(200, { "Content-Type": "application/text" });
-    res.end("API is running...");
-  } else if (req.url === "/api/posts" && req.method === "GET") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(posts));
-  } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "Route not found!" }));
-  }
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.write("<h1>Hello nodejs api</h1>");
+  res.end();
 });
 
 const PORT = process.env.PORT || 5000;
